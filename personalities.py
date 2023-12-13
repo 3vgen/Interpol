70058b33625eb7d7e580154a00b5deb8f2b6bddd
Evgeniy Semin <evgeniy.zhenia@gmail.com>
2023-12-13 17:13:47 +0300

    acsess to change personal data

--- personalities.py
+++ personalities.py
@@ -6,12 +6,13 @@ import textwrap
 
 class SubRoot:
     def __init__(self, id_):
-        self.id = id_
+        self.add_button = None
+        self.ident = id_
 
-        print(self.id)
         self.label = None
         self.win = None
         self.data_frame = None
+        self.button_frame = None
 
         self.gender_label = None
         self.gender_entry = None
@@ -37,53 +38,107 @@ class SubRoot:
         self.languages_spoken_entry = None
         self.languages_spoken_label = None
 
+    def change_data(self):
+        conn = sqlite3.connect('Interpol.db')
+        c = conn.cursor()
+        print(self.charges_entry.get('1.0', END))
+        c.execute(f"UPDATE personal_data SET gender = :gender, height = :height, hair_colour = :hair_colour, "
+                  f"eyes_colour = :eyes_colour, languages_spoken = :languages_spoken, charges = :charges,"
+                  f"nickname = :nickname, place_of_birth = :place_of_birth WHERE id_person = :id",
+                  {
+                      'gender': self.gender_entry.get(),
+                      'height': self.height_entry.get(),
+                      'hair_colour': self.h_colour_entry.get(),
+                      'eyes_colour': self.e_colour_entry.get(),
+                      'languages_spoken': self.languages_spoken_entry.get(),
+                      'charges': self.charges_entry.get('1.0', END),
+                      'nickname': self.nickname_entry.get(),
+                      'place_of_birth': self.place_of_birth_entry.get('1.0', END),
+                      'id': self.ident
+                  })
+
+        conn.commit()
+        conn.close()
+
+    def record_personal_info(self):
+        conn = sqlite3.connect('Interpol.db')
+        c = conn.cursor()
+
+        c.execute(f"SELECT * FROM personal_data WHERE id_person = :id",
+                  {
+                      'id': self.ident
+                  })
+
+        records = c.fetchall()
+        print(records[0])
+        self.gender_entry.insert(0, records[0][1])
+        self.height_entry.insert(0, records[0][2])
+        self.h_colour_entry.insert(0, records[0][3])
+        self.e_colour_entry.insert(0, records[0][4])
+        self.languages_spoken_entry.insert(0, records[0][5])
+        self.place_of_birth_entry.insert('1.0', records[0][6])
+        self.charges_entry.insert('1.0', records[0][7])
+        self.nickname_entry.insert(0, records[0][8])
+        conn.commit()
+        conn.close()
+
     def run(self):
         self.win = Toplevel()
         self.win.geometry('800x1000')
         self.label = Label(self.win, text='Личные данные преступника', font='Arial 15 bold', fg='Black')
         self.label.pack()
-        self.data_frame = LabelFrame(self.win, text="Данные")
-        self.data_frame.pack()
+
+        self.data_frame = LabelFrame(self.win, text="Данные", background='#B6D7E4')
+        self.data_frame.pack(fill=X)
+
+        self.button_frame = LabelFrame(self.win, text="Действия")
+        self.button_frame.pack()
+
+        self.add_button = Button(self.button_frame, text="Добавить", command=self.change_data)
+        self.add_button.grid(row=0, column=0, padx=10, pady=10)
 
         self.gender_label = Label(self.data_frame, text="Пол")
-        self.gender_label.grid(row=0, column=0, padx=10, pady=10)
+        self.gender_label.grid(row=0, column=0, padx=10, pady=10, sticky='W')
         self.gender_entry = Entry(self.data_frame, width=10)
-        self.gender_entry.grid(row=0, column=1, padx=10, pady=10)
+        self.gender_entry.grid(row=0, column=1, padx=10, pady=10, sticky='W')
 
         self.height_label = Label(self.data_frame, text="Рост")
-        self.height_label.grid(row=1, column=0, padx=10, pady=10)
+        self.height_label.grid(row=1, column=0, padx=10, pady=10, sticky='W')
         self.height_entry = Entry(self.data_frame)
-        self.height_entry.grid(row=1, column=1, padx=10, pady=10)
+        self.height_entry.grid(row=1, column=1, padx=10, pady=10, sticky='W')
 
         self.h_colour_label = Label(self.data_frame, text="Цвет волос")
-        self.h_colour_label.grid(row=2, column=0, padx=10, pady=10)
+        self.h_colour_label.grid(row=2, column=0, padx=10, pady=10, sticky='W')
         self.h_colour_entry = Entry(self.data_frame)
-        self.h_colour_entry.grid(row=2, column=1, padx=10, pady=10)
+        self.h_colour_entry.grid(row=2, column=1, padx=10, pady=10, sticky='W')
 
         self.e_colour_label = Label(self.data_frame, text="Цвет глаз")
-        self.e_colour_label.grid(row=3, column=0, padx=10, pady=10)
+        self.e_colour_label.grid(row=3, column=0, padx=10, pady=10, sticky='W')
         self.e_colour_entry = Entry(self.data_frame)
-        self.e_colour_entry.grid(row=3, column=1, padx=10, pady=10)
+        self.e_colour_entry.grid(row=3, column=1, padx=10, pady=10, sticky='W')
 
         self.languages_spoken_label = Label(self.data_frame, text="Языки")
-        self.languages_spoken_label.grid(row=4, column=0, padx=10, pady=10)
+        self.languages_spoken_label.grid(row=4, column=0, padx=10, pady=10, sticky='W')
         self.languages_spoken_entry = Entry(self.data_frame)
-        self.languages_spoken_entry.grid(row=4, column=1, padx=10, pady=10)
+        self.languages_spoken_entry.grid(row=4, column=1, padx=10, pady=10, sticky='W')
 
         self.nickname_label = Label(self.data_frame, text="Кличка")
-        self.nickname_label.grid(row=5, column=0, padx=10, pady=10)
+        self.nickname_label.grid(row=5, column=0, padx=10, pady=10, sticky='W')
         self.nickname_entry = Entry(self.data_frame)
-        self.nickname_entry.grid(row=5, column=1, padx=10, pady=10)
+        self.nickname_entry.grid(row=5, column=1, padx=10, pady=10, sticky='W')
 
         self.place_of_birth_label = Label(self.data_frame, text="Место рождения")
-        self.place_of_birth_label.grid(row=6, column=0, padx=10, pady=10)
-        self.place_of_birth_entry = Entry(self.data_frame)
-        self.place_of_birth_entry.grid(row=6, column=1, padx=10, pady=10)
+        self.place_of_birth_label.grid(row=6, column=0, padx=10, pady=10, sticky='W')
+        self.place_of_birth_entry = Text(self.data_frame, height=10, width=50)
+        self.place_of_birth_entry.grid(row=6, column=1, padx=10, pady=10, sticky='W')
 
         self.charges_label = Label(self.data_frame, text="Отличительные черты")
-        self.charges_label.grid(row=7, column=0, padx=10, pady=10)
+        self.charges_label.grid(row=7, column=0, padx=10, pady=10, sticky='W')
         self.charges_entry = Text(self.data_frame, height=10, width=50)
-        self.charges_entry.grid(row=7, column=1, padx=10, pady=10)
+        self.charges_entry.grid(row=7, column=1, padx=10, pady=10, sticky='W')
+        self.record_personal_info()
+
+
 # def open_win():
 #     win = Toplevel()
 #     win.geometry('600x400')
@@ -132,53 +187,53 @@ class Personalities:
         self.person_tree['columns'] = ("id", "forename", "family_name", "date_of_birth", "nationality")
         self.run_personalities()
 
-    def on_column_click(self, event):
-        self.remove_all()
-        col_index = self.person_tree.identify_column(event.x)
-        conn = sqlite3.connect('Interpol.db')
-        c = conn.cursor()
-        global records
-        # print("Нажатие на колонку номер", col_index.index(1))
-        if col_index == '#1':
-            c.execute("""SELECT *
-                           FROM person
-                           OREDER BY id;
-                           """)
-            records = c.fetchall()
-        if col_index == '#2':
-            c.execute("""SELECT crime.id, date_of_crime, place_of_crime, type_of_crime.name
-                    FROM crime
-                    join type_of_crime on crime.type_c = type_of_crime.id order by date_of_crime desc;
-                    """)
-            records = c.fetchall()
-
-        if col_index == '#3':
-            c.execute("""SELECT crime.id, date_of_crime, place_of_crime, type_of_crime.name
-                    FROM crime
-                    join type_of_crime on crime.type_c = type_of_crime.id order by place_of_crime;
-                    """)
-            records = c.fetchall()
-
-        if col_index == '#4':
-            c.execute("""SELECT crime.id, date_of_crime, place_of_crime, type_of_crime.name
-                    FROM crime
-                    join type_of_crime on crime.type_c = type_of_crime.id order by type_c;
-                    """)
-            records = c.fetchall()
-
-        global count
-        count = 0
-        for record in records:
-            if count % 2 == 0:
-                self.person_tree.insert(parent='', index='end', iid=count, text='',
-                                        values=(record[0], record[1], record[2], record[3]), tags=('evenrow',))
-            else:
-                self.person_tree.insert(parent='', index='end', iid=count, text='',
-                                        values=(record[0], record[1], record[2], record[3]), tags=('oddrow',))
-            count += 1
-
-        conn.commit()
-        conn.close()
+    # def on_column_click(self, event):
+    #     self.remove_all()
+    #     col_index = self.person_tree.identify_column(event.x)
+    #     conn = sqlite3.connect('Interpol.db')
+    #     c = conn.cursor()
+    #     global records
+    #     # print("Нажатие на колонку номер", col_index.index(1))
+    #     if col_index == '#1':
+    #         c.execute("""SELECT *
+    #                        FROM person
+    #                        OREDER BY id;
+    #                        """)
+    #         records = c.fetchall()
+    #     if col_index == '#2':
+    #         c.execute("""SELECT crime.id, date_of_crime, place_of_crime, type_of_crime.name
+    #                 FROM crime
+    #                 join type_of_crime on crime.type_c = type_of_crime.id order by date_of_crime desc;
+    #                 """)
+    #         records = c.fetchall()
+    #
+    #     if col_index == '#3':
+    #         c.execute("""SELECT crime.id, date_of_crime, place_of_crime, type_of_crime.name
+    #                 FROM crime
+    #                 join type_of_crime on crime.type_c = type_of_crime.id order by place_of_crime;
+    #                 """)
+    #         records = c.fetchall()
+    #
+    #     if col_index == '#4':
+    #         c.execute("""SELECT crime.id, date_of_crime, place_of_crime, type_of_crime.name
+    #                 FROM crime
+    #                 join type_of_crime on crime.type_c = type_of_crime.id order by type_c;
+    #                 """)
+    #         records = c.fetchall()
+    #
+    #     global count
+    #     count = 0
+    #     for record in records:
+    #         if count % 2 == 0:
+    #             self.person_tree.insert(parent='', index='end', iid=count, text='',
+    #                                     values=(record[0], record[1], record[2], record[3]), tags=('evenrow',))
+    #         else:
+    #             self.person_tree.insert(parent='', index='end', iid=count, text='',
+    #                                     values=(record[0], record[1], record[2], record[3]), tags=('oddrow',))
+    #         count += 1
+    #
+    #     conn.commit()
+    #     conn.close()
 
     def remove_all(self):
         for record in self.person_tree.get_children():
@@ -252,7 +307,10 @@ class Personalities:
         self.f_entry.insert(0, values[2])
         self.db_entry.insert(0, values[3])
         self.nt_entry.insert(0, values[4])
+
+    def open_add_window(self):
         self.sb_rt = SubRoot(self.id_entry.get())
+        self.sb_rt.run()
 
     def record_data(self):
         conn = sqlite3.connect('Interpol.db')
@@ -343,7 +401,8 @@ class Personalities:
         self.remove_button.grid(row=0, column=2, padx=10, pady=10)
 
         self.sb_rt = SubRoot(self.id_entry.get())
-        self.select_button = Button(self.button_frame, text="Открыть подробную информацию", command=self.sb_rt.run)
+        self.select_button = Button(self.button_frame, text="Открыть подробную информацию",
+                                    command=self.open_add_window)
         self.select_button.grid(row=0, column=3, padx=10, pady=10)
 
         self.person_tree.pack(pady=20)
